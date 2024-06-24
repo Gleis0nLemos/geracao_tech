892ac7d7839d2070214408b38de2b7120c5a6df2
Gleison Lemos <gleison04lemos@gmail.com>
2024-06-24 18:00:25 -0300

    feat: aula 03, dia 24-06

---
+++ 24-06/javascript-aula02/index.js
@@ -0,0 +1,11 @@
+let nome; // declaração
+nome = "Gleison"; // string
+let idade = 23; // number
+let estaAprovado = true; // boolean
+let sobrenome = undefined; // undefined
+let corSelecionada = null; // null
+
+nome = "João"; // reatribuição
+
+const PI = 3.14; // constante
+alert(PI);
