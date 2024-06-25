4c8586dff4ed3759f6d0b77640b7778adf24fe7b
Gleison Lemos <gleison04lemos@gmail.com>
2024-06-25 20:44:13 -0300

    feat: aula 03, dia 25-06

--- 25-06/funcoes/index.js
+++ 25-06/funcoes/index.js
@@ -6,12 +6,21 @@
 // boasVindas("João");
 // boasVindas("Maria");
 
-function somar(num1, num2) {
-  document.write(`<h1>A soma é: ${num1 + num2}</h1>`);
-  return num1 + num2;
+// function somar(num1, num2) {
+//   document.write(`<h1>A soma é: ${num1 + num2}</h1>`);
+//   return num1 + num2;
+// }
+
+// somar(10, 5)
+// somar(5, 10)
+
+// document.write(`<h1>A divisão é: ${somar(10, 50)}</h1>`)
+
+function datas(tag, limite) {
+  for(let i = 1; i < limite; i++) {
+    tag.innerHTML += `<option>${i}</option>`
+  }
 }
 
-somar(10, 5)
-somar(5, 10)
-
-document.write(`<h1>A divisão é: ${somar(10, 50)}</h1>`)
+datas(dia, 32)
+datas(mes, 13)
