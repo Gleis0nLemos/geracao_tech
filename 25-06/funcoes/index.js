877fd60829d6d7015bc4ba50aa560d675baae957
Gleison Lemos <gleison04lemos@gmail.com>
2024-06-25 19:48:40 -0300

    feat: aula 02, dia 25-06

--- 25-06/funcoes/index.js
+++ 25-06/funcoes/index.js
@@ -1,5 +1,17 @@
-function boasVindas(nome) {
-  document.write(`<h1>Olá ${nome}</h1>`)
+// function boasVindas(nome) {
+//   document.write(`<h1>Olá ${nome}</h1>`)
+// }
+
+// boasVindas("Gleison");
+// boasVindas("João");
+// boasVindas("Maria");
+
+function somar(num1, num2) {
+  document.write(`<h1>A soma é: ${num1 + num2}</h1>`);
+  return num1 + num2;
 }
 
-boasVindas("Gleison");
+somar(10, 5)
+somar(5, 10)
+
+document.write(`<h1>A divisão é: ${somar(10, 50)}</h1>`)
