32bdac5d7a0dc9dab24e04106f999c03503fbf26
Gleison Lemos <gleison04lemos@gmail.com>
2024-06-26 19:52:49 -0300

    feat: aula 01, dia 26-06

---
+++ 26-06/condicionais/index.js
@@ -0,0 +1,10 @@
+let num1 = Number(prompt("Numero 1"));
+let num2 = Number(prompt("Numero 2"));
+
+if (num1 > 5) {
+  console.log("É maior que 5");
+} else if (num1 < 5) {
+  console.log("É menor que 5");
+} else {
+  console.log("É igual a 5");
+}
