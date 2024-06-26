a76dabe5545176b18455c8bbfdfa2679125bed7e
Gleison Lemos <gleison04lemos@gmail.com>
2024-06-26 15:46:20 -0300

    aula 04, dia 25-06

---
+++ 25-06/objetos/index.js
@@ -0,0 +1,25 @@
+let pessoa = {
+  nome: "Gleison",
+  idade: 23,
+  genero: "Masculino",
+  habilidades : [
+    "PHP", 
+    "Laravel", 
+    "SQL"
+  ],
+  andar: () => {
+    console.log("andei")
+  },
+  rosto: {
+    olhos: "castanhos",
+    temCabelo: true
+  }
+}
+
+// exemplo de funcao anonima, o nome da function é a variavel q a atribui
+let subtrair = function() {
+  console.log("Subtraiu")
+}
+
+// exemplo de arrow function, funciona da mesma forma de uma função anônima, porém, mais reduzida
+let multiplicar = (a, b) => {return a * b}
