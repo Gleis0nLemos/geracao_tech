720b697ee7017cb0e6ee9e1612b6b1f363a4394e
Gleison Lemos <gleison04lemos@gmail.com>
2024-06-26 16:43:59 -0300

    aula 05, dia 25-06

---
+++ 25-06/dom/index.js
@@ -0,0 +1,9 @@
+localStorage.setItem('nome', 'Gleison');
+
+setTimeout(() => {
+  alert("Olá pessoal")
+}, 3000);
+
+setInterval(() => {
+  console.log("Oi")
+}, 3000)
--- 25-06/objetos/index.js
+++ 25-06/objetos/index.js
@@ -12,7 +12,7 @@ let pessoa = {
   },
   rosto: {
     olhos: "castanhos",
-    temCabelo: true
+    temCabelo: true,
   }
 }
 
