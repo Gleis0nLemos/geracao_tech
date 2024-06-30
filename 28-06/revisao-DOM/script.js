541efeaa31057c9c654e052e20a4ae75109e8360
Gleison Lemos <gleison04lemos@gmail.com>
2024-06-30 12:07:45 -0300

    feat: revisoes aula ao vivo, dia 28-06

---
+++ 28-06/revisao-DOM/script.js
@@ -0,0 +1,7 @@
+const titulo1 = document.getElementById('titulo1');
+
+const titulos = document.getElementsByClassName('titulo2');
+
+titulos[0].innerHTML = `<em>Texto qualquer</em>`
+
+console.log(titulos)
