0bfb0e7617cf59d67f3c46955f43d2098b9dde82
Gleison Lemos <gleison04lemos@gmail.com>
2024-07-01 17:55:18 -0300

    feat: exemplo prático, dia 27-06

---
+++ 27-06/for-do-while/scripts.js
@@ -0,0 +1,24 @@
+// let numeros = [1, 2, 3, 4, 5];
+
+// for (let i = 0; i < numeros.length; i++) {
+//   numeros[i] += 1;
+// }
+
+// console.log(numeros);
+
+// let numeros = [1, 2, 3, 4, 5];
+
+// for (let i = 0; i < numeros.length; i++) {
+//   numeros[i] += 2;
+// }
+
+// console.log(numeros);
+
+let i = 0; 
+
+do { 
+	console.log(i);
+	i++; 
+} while (i < 5);
+
+console.log(i);
