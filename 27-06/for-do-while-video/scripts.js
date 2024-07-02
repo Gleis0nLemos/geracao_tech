c2a3e82a31499f7f6a672edb1321fa4b880ec7f3
Gleison Lemos <gleison04lemos@gmail.com>
2024-07-02 11:42:25 -0300

    feat: aula 02, dia 27-06

---
+++ 27-06/for-do-while-video/scripts.js
@@ -0,0 +1,13 @@
+let nome = "Gleison";
+let nome1 = "Márcio";
+let nome2 = "Alessandro";
+let nome3 = "Glauber";
+
+let nomes = ["Gleidson", "Glauber", "Alessandro", "Márcio"];
+
+let numeros = [1, 2, 3, 4, 5];
+
+for (let i = 0; i < numeros.length; i++) {
+  numeros[i] += 1;
+  console.log("volta:", i);
+}
