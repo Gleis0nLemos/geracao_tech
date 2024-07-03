607ba8d838091db097d8e1e40b98070f8d600777
Gleison Lemos <gleison04lemos@gmail.com>
2024-07-03 00:24:49 -0300

    aula 2 api, dia 28-06

---
+++ 28-06/requisicoesHTTP-video2/index.js
@@ -0,0 +1,31 @@
+function buscarRegioes() {
+  fetch(`https://servicodados.ibge.gov.br/api/v1/localidades/regioes`)
+  .then(res => res.json())
+  .then(res => {
+    res.map(regiao => {
+      regioes.innerHTML += `<option value="${regiao.id}">${regiao.nome}</option>`
+    })
+  })
+} buscarRegioes();
+
+function buscarEstados() {
+  fetch(`https://servicodados.ibge.gov.br/api/v1/localidades/regioes/${regioes.value}/estados`)
+  .then(res => res.json())
+  .then(res => {
+    estados.innerHTML = "<option value=''>Selecione um estado</option>";
+    res.map(estado => {
+      estados.innerHTML += `<option value="${estado.id}">${estado.nome}</option>`
+    })
+  })
+}
+
+function buscarCidades() {
+  fetch(`https://servicodados.ibge.gov.br/api/v1/localidades/estados/${estados.value}/municipios`)
+  .then(res => res.json())
+  .then(res => {
+    cidades.innerHTML = "<option value=''>Selecione uma cidade</option>";
+    res.map(cidade => {
+      cidades.innerHTML += `<option value="${cidade.id}">${cidade.nome}</option>`
+    })
+  })
+}
