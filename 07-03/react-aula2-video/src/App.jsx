20245e36d513e961a36c3d605eaa21164a1ab93e
Gleison Lemos <gleison04lemos@gmail.com>
2024-07-08 19:21:26 -0300

    feat: aula 3, dia 03-07

--- 07-03/react-aula2-video/src/App.jsx
+++ 07-03/react-aula2-video/src/App.jsx
@@ -1,5 +1,6 @@
 
 import Banner from "./components/Banner";
+import FormaDeBolo from "./components/FormaDeBolo";
 import Header from "./components/Header";
 import { Componente1, Componente2, Componente3 } from "./components/VariosComponentes";
 
@@ -22,11 +23,13 @@ const App = () => {
     
     */
     <>
-      <Header />  
+      {/* <Header />  
       <Banner />
       <Componente1 />
       <Componente2 />
-      <Componente3 />
+      <Componente3 /> */}
+      <FormaDeBolo sabor="Laranja" cobertura="Chocolate"/>
+      <FormaDeBolo sabor="Chocolate" cobertura="Chantilly"/>  
     </>
   );
 }
