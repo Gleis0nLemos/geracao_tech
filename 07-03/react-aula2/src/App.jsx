35d4b20971ba5c0761fbd2071c87e5318a3bb55f
Gleison Lemos <gleison04lemos@gmail.com>
2024-07-08 12:57:26 -0300

    feat: import e export, React, dia 03-07

---
+++ 07-03/react-aula2/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Header from './components/Header';
+import Banner from './components/Banner';
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <Header />
+        <Banner />
+      </div>
+    </>
+  )
+}
+
+export default App
