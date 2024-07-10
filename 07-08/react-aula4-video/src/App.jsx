b9bd4105f5cf90b1a92226e5f33caf7e2ed61147
Gleison Lemos <gleison04lemos@gmail.com>
2024-07-10 08:37:22 -0300

    feat: aula 2, dia 08-07

--- 07-08/react-aula4-video/src/App.jsx
+++ 07-08/react-aula4-video/src/App.jsx
@@ -1,37 +1,9 @@
-import { useState } from "react";
-import { useEffect } from "react"
-
+import Paths from "./routes/Paths"
 
 const App = () => {
-  const [count, setCount] = useState(0);
-  const [count2, setCount2] = useState(0);
-
-  function boasVindas() {
-    alert("Boas Vindas");
-  }
-
-  useEffect(() => {
-    console.log("useEffect foi chamado")
-  });
-
-  useEffect(() => {
-    boasVindas();
-    console.log("useEffect que é chamado apenas uma vez")
-  }, [])
-
-  useEffect(() => {
-    console.log('useEffect ativado somente por dependências');
-  }, [count])
-
   return (
-    <> 
-      <h1>Hello, World!</h1>
-      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Provident nam, voluptates eveniet, amet non dolorum natus, dolorem voluptatibus aliquid temporibus modi nobis est vero. Ea distinctio rem ab iusto commodi?</p>
-
-      <h2>Contador: {count}</h2>
-      <button onClick={() => setCount(count + 1)}>add</button>
-      <h2>Contador: {count2}</h2>
-      <button onClick={() => setCount2(count2 + 1)}>add</button>
+    <>
+      <Paths />
     </>
   )
 }
---
+++ 07-08/react-aula4-video/src/routes/Paths.jsx
@@ -0,0 +1,18 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Home from '../pages/Home';
+import Products from '../pages/Products';
+
+const Paths = () => {
+  return (  
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home />}/>
+          <Route path='/products' element={<Products />}/>
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
+ 
+export default Paths;
