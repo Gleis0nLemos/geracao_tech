c0d75d72b1855611e2a1fc98d4c397f8ecedeba7
Gleison Lemos <gleison04lemos@gmail.com>
2024-07-10 07:50:35 -0300

    feat: usando react-router-dom, dia 08-07

--- 07-08/react-aula4/src/App.jsx
+++ 07-08/react-aula4/src/App.jsx
@@ -1,21 +1,14 @@
-import React, { useState, useEffect } from 'react'
 import './App.css'
+import Header from './components/Header'
+import AppRoutes from './routes/Routes'
 
 function App() {
-  const [count, setCount] = useState(0)
-  const [count2, setCount2] = useState(0)
-
-  useEffect(() => {
-    console.log('useEffect foi chamado')
-  }, [count])
   return (
-    <>
+    <> 
       <div>
-        <p>Contador 1: {count}</p>
-        <button onClick={() => setCount(count + 1)}>Incrementar Contador 1</button>
-        <p>Contador 2: {count2}</p>
-        <button onClick={() => setCount2(count2 + 1)}>Incrementar Contador 2</button>
-      </div>
+        {/* <Header /> */}
+        <AppRoutes />
+      </div> 
     </>
   )
 }
---
+++ 07-08/react-aula4/src/routes/Routes.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Home from "../pages/Home";
+import Products from "../pages/Products";
+import Header from '../components/Header';
+
+const AppRoutes = () => {
+  return (  
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/products" element={<Products />} />
+      </Routes>
+    </Router>
+  );
+}
+ 
+export default AppRoutes;
