17c6c1b43c80e9102399eae2612a7c04c67aee84
Gleison Lemos <gleison04lemos@gmail.com>
2024-07-10 11:39:41 -0300

    feat: tratamento de rotas, dia 08-07

--- 07-08/react-aula4/src/pages/Products.jsx
+++ 07-08/react-aula4/src/pages/Products.jsx
@@ -1,7 +1,38 @@
+import { Link } from "react-router-dom";
+
 const Products = () => {
+  const productList = [
+    {
+      id: 1,
+      name: "Product 1"
+    },
+    {
+      id: 2,
+      name: "Product 2"
+    },
+    {
+      id: 3,
+      name: "Product 3"
+    },
+    {
+      id: 4,
+      name: "Product 4"
+    },
+    {
+      id: 5,
+      name: "Product 5"
+    }
+  ]
+
   return (  
     <div>
       <h1>Products</h1>
+      <ul>{productList.map(product => (
+        <li key={product.id}>
+          <Link to={`/products/${product.id}/${product.name}`}>{product.name}</Link>
+        </li>
+      ))}
+      </ul>
     </div>
   );
 }
--- 07-08/react-aula4/src/routes/Routes.jsx
+++ 07-08/react-aula4/src/routes/Routes.jsx
@@ -3,17 +3,23 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Home from "../pages/Home";
 import Products from "../pages/Products";
 import PageLayout from '../layouts/PageLayout';
+import NotFound from '../components/NotFound';
+import Product from '../components/Product';
 
 const AppRoutes = () => {
   return (  
     <Router>
       <Routes>
         <Route path="/" element={<PageLayout />}>
-          <Route path="/" element={<Home />}
+          <Route index element={<Home />}
             />
           <Route path="/products" element={<Products />} 
             />
-        </Route>   
+          <Route path="/products/:id/:name" element={<Product />}
+            />
+        </Route> 
+        <Route path="*" element={<NotFound />}
+            />  
       </Routes>
     </Router>
   );
