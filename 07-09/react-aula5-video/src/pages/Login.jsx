cd3367c0be3a87db57125ebb1516b0189db9aab5
Gleison Lemos <gleison04lemos@gmail.com>
2024-07-11 13:23:36 -0300

    feat: aula 5, dia 09-07

--- 07-09/react-aula5-video/src/pages/Login.jsx
+++ 07-09/react-aula5-video/src/pages/Login.jsx
@@ -2,12 +2,16 @@ import { InputText } from 'primereact/inputtext';
 import { InputIcon } from 'primereact/inputicon';
 import { IconField } from 'primereact/iconfield';
 import { Button } from 'primereact/button';
+import { useState } from 'react';
 
 const Login = () => {
+
+  const [mostrarSenha, setMostrarSenha] = useState(false)
+
   return (  
     <>
       <div className='bg-primary-500 h-screen flex align-items-center justify-content-center px-3'>
-        <form className='col-12 md:col-3 xl:col-3 surface-0 p-3 border-round-md'>
+        <form className='col-12 md:col-4 xl:col-3 surface-0 p-3 border-round-md'>
           <h3 className='text-center text-4xl'>Seja bem-vindo</h3>
           <label htmlFor="email" className='block uppercase font-bold text-sm mb-1'>Email</label>
           <InputText 
@@ -19,10 +23,14 @@ const Login = () => {
           <label htmlFor="senha" className='block uppercase font-bold text-sm mb-1' >Senha</label>
           <div className='mb-3'>
             <IconField>
-              <InputIcon className="pi pi-eye" />
+              <InputIcon 
+                className={`pi ${ mostrarSenha ? 'pi-eye' : 'pi-eye-slash'} cursor-pointer`}
+                onClick={() => setMostrarSenha(!mostrarSenha)}
+                />
               <InputText 
                 id='senha'
                 placeholder='******'
+                type={ mostrarSenha ? 'text' : 'password' }
                 className='w-full'
                   />
             </IconField>
