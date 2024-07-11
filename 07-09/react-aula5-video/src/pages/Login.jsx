343aa39add6de782e16999dfc49be2127ee88c2d
Gleison Lemos <gleison04lemos@gmail.com>
2024-07-11 12:21:44 -0300

    feat: aula 4, dia 09-07

--- 07-09/react-aula5-video/src/pages/Login.jsx
+++ 07-09/react-aula5-video/src/pages/Login.jsx
@@ -6,26 +6,32 @@ import { Button } from 'primereact/button';
 const Login = () => {
   return (  
     <>
-      <div>
-        <form>
-          <h3>Seja bem-vindo</h3>
-          <label htmlFor="email">Email</label>
+      <div className='bg-primary-500 h-screen flex align-items-center justify-content-center px-3'>
+        <form className='col-12 md:col-3 xl:col-3 surface-0 p-3 border-round-md'>
+          <h3 className='text-center text-4xl'>Seja bem-vindo</h3>
+          <label htmlFor="email" className='block uppercase font-bold text-sm mb-1'>Email</label>
           <InputText 
             id='email'
             type='email'
             placeholder='email@email.com'
+            className='mb-3 w-full'
             />
-          <label htmlFor="senha">Senha</label>
-          <IconField>
-            <InputIcon className="pi pi-eye" />
-            <InputText 
-              id='senha'
-              placeholder='******'
-               />
-          </IconField>
+          <label htmlFor="senha" className='block uppercase font-bold text-sm mb-1' >Senha</label>
+          <div className='mb-3'>
+            <IconField>
+              <InputIcon className="pi pi-eye" />
+              <InputText 
+                id='senha'
+                placeholder='******'
+                className='w-full'
+                  />
+            </IconField>
+          </div>
+          
           <Button 
             type="submit"
-            label="Entrar" 
+            label="Entrar"
+            className="w-full"
             />
         </form>
       </div>
