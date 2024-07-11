de2c23da9de9fd908c88307b625b0fe3877e1c22
Gleison Lemos <gleison04lemos@gmail.com>
2024-07-11 14:44:08 -0300

    feat: PrimeReact, PrimeFlex, PrimeIcon, usando DataTable, Column. dia 09-07

--- 07-09/react-aula5/src/App.jsx
+++ 07-09/react-aula5/src/App.jsx
@@ -1,50 +1,38 @@
 import "primereact/resources/themes/lara-light-blue/theme.css"; // Tema PrimeReact
 import "primeicons/primeicons.css"; // Ïcones PrimeReact
 import "primeflex/primeflex.css"; // PrimeFlex para utilitários CSS
-// import 'primereact/resources/primereact.min.css'; // Estilos PrimeReact
-// import Paths from "./routes/Paths"; 
+// import 'primereact/resources/primereact.min.css'; // Estilos PrimeReact 
 
-import { Button } from 'primereact/button';
-import { Card } from 'primereact/card';
+import React, { useState, useEffect } from 'react';
+import { DataTable } from 'primereact/datatable';
+import { Column } from 'primereact/column';
 
 const App = () => {
 
-  const header = <img alt="Card" src="https://primefaces.org/cdn/primereact/images/usercard.png" />;
+  const [products, setProducts] = useState([]);
 
-  const footer = (
-    <span>
-      <Button 
-        label="Salvar" 
-        icon="pi pi-check"
-        />
-      <Button 
-        label="Cancelar" 
-        icon="pi pi-times"
-        className="p-button-secondary"
-        />
-    </span>
-  )
+  useEffect(() => {
+    fetch('https://fakestoreapi.com/products')
+    .then((res) => res.json())
+    .then((data) => setProducts(data))
+  }, []);
 
   return (  
     <>
-      <div className="flex justify-content-center align-items-center" 
-        style={{ 
-          height: '100vh' 
-        }}> 
-        <Card
-          title="Título do Card"
-          subTitle="Subtítulo"
-          style={{ width: '25em' }}
-          header={header}
-          footer={footer}
-          >
-          <p
-            className="m-0"
-            style={{ lineHeight: '1.5'}}
-            >
-            Este é um exemplo de conteúdo dentro de um card usando PrimeReact
-          </p>
-        </Card>
+      <div 
+        className="flex justify-content-center align-items-center" 
+        style={{ height: '100vh' }}
+        > 
+        <DataTable
+          value={products}
+          paginator
+          rows={10}
+          className="datatable-sm"
+          > 
+          <Column field="title" header="Produto" />
+          <Column field="price" header="Preço" />
+          <Column field="category" header="Categoria" />
+        </DataTable>
       </div> 
     </>
   );
