b0e7bee982c79ee74255f7f0dd5b82dba285dc2e
Gleison Lemos <gleison04lemos@gmail.com>
2024-07-11 14:22:04 -0300

    feat: PrimeReact, PrimeFlex, PrimeIcon, usando Card. dia 09-07

--- 07-09/react-aula5/src/App.jsx
+++ 07-09/react-aula5/src/App.jsx
@@ -1,8 +1,51 @@
+import "primereact/resources/themes/lara-light-blue/theme.css"; // Tema PrimeReact
+import "primeicons/primeicons.css"; // Ïcones PrimeReact
+import "primeflex/primeflex.css"; // PrimeFlex para utilitários CSS
+// import 'primereact/resources/primereact.min.css'; // Estilos PrimeReact
+// import Paths from "./routes/Paths"; 
+
+import { Button } from 'primereact/button';
+import { Card } from 'primereact/card';
+
 const App = () => {
+
+  const header = <img alt="Card" src="https://primefaces.org/cdn/primereact/images/usercard.png" />;
+
+  const footer = (
+    <span>
+      <Button 
+        label="Salvar" 
+        icon="pi pi-check"
+        />
+      <Button 
+        label="Cancelar" 
+        icon="pi pi-times"
+        className="p-button-secondary"
+        />
+    </span>
+  )
 
   return (  
     <>
-      
+      <div className="flex justify-content-center align-items-center" 
+        style={{ 
+          height: '100vh' 
+        }}> 
+        <Card
+          title="Título do Card"
+          subTitle="Subtítulo"
+          style={{ width: '25em' }}
+          header={header}
+          footer={footer}
+          >
+          <p
+            className="m-0"
+            style={{ lineHeight: '1.5'}}
+            >
+            Este é um exemplo de conteúdo dentro de um card usando PrimeReact
+          </p>
+        </Card>
+      </div> 
     </>
   );
 }
