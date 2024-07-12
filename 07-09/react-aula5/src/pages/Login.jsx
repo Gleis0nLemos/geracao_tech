f9dfb2fd0bb1f750e9d28a66f5d33bc2ebc06770
Gleison Lemos <gleison04lemos@gmail.com>
2024-07-11 23:32:07 -0300

    feat: usando InputText, InputIcon, IconField, e usaNavigate, dia 09-07

--- 07-09/react-aula5/src/pages/Login.jsx
+++ 07-09/react-aula5/src/pages/Login.jsx
@@ -1,10 +1,13 @@
 import { InputText } from 'primereact/inputtext';
-import { Password } from 'primereact/password';
+import { InputIcon } from 'primereact/inputicon';
+import { IconField } from 'primereact/iconfield';
 import { Button } from 'primereact/button';
 import { useNavigate } from 'react-router-dom';
-import 'primeflex/primeflex.css';
+import { useState } from 'react';
 
 const Login = () => {
+
+  const [mostrarSenha, setMostrarSenha] = useState(false)
 
   const navigate = useNavigate();
 
@@ -16,12 +19,11 @@ const Login = () => {
   return (  
     <>
       <div
-        className='bg-primary-500 h-screen flex justify-content-center align-items-center'
+        className='bg-primary-500 h-screen flex justify-content-center align-items-center px-3'
         style={{ height: '100vh' }} 
         >
         <div
-          className='card shadow-5 p-4 surface-0 border-round-md' 
-          style={{ width: '300px' }}        
+          className='col-12 md:col-4 xl:col-3 card shadow-5 p-4 surface-0 border-round-md'         
           >
           <h3 className='text-center text-2xl'>Seja Bem-vindo</h3>
           <form onSubmit={handleSubmit}>
@@ -34,19 +36,25 @@ const Login = () => {
                 className='inputtext-sm mb-3 w-full'
                 />
             </div>
-            <div className='field mb-3'>
-              <label htmlFor="password" className='block uppercase font-bold text-sm mb-1'>Senha</label>
-              <Password 
-                id="password" 
-                placeholder="**********"
-                toggleMask 
-                feedback={false}
-                />
+            <label htmlFor="senha" className='block uppercase font-bold text-sm mb-1' >Senha</label>
+            <div className='mb-3'>
+              <IconField>
+                <InputIcon 
+                  className={`pi ${ mostrarSenha ? 'pi-eye' : 'pi-eye-slash'} cursor-pointer`}
+                  onClick={() => setMostrarSenha(!mostrarSenha)}
+                  />
+                <InputText 
+                  id='senha'
+                  placeholder='******'
+                  type={ mostrarSenha ? 'text' : 'password' }
+                  className='w-full'
+                    />
+              </IconField>
             </div>
             <Button 
               label="Entrar"
               type="submit"
-              className="button-sm button-primary"
+              className="button-sm button-primary w-full"
               />
           </form>
         </div>
