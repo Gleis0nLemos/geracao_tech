d61a361a9588f8431246dd4020f3a485cca96163
Gleison Lemos <gleison04lemos@gmail.com>
2024-07-22 18:08:27 -0300

    feat: aula 02, dia 16-07

--- 07-16/aula1-video/server.js
+++ 07-16/aula1-video/server.js
@@ -1,1 +1,12 @@
-console.log("Olá, eu sou o server.js");
+const { createServer } = require('node:http')
+const host = '127.0.0.1';
+const port = 3000;
+
+const server = createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/html' })
+  res.end(('<h1>Hello World!</h1> <br> Aula de Backend'))
+})
+
+server.listen(port, host, () => {
+  console.log(`Server running at http://${host}:${3000}`);
+})
