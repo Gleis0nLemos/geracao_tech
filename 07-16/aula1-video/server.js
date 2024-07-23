0146d7580dffb3c72a4eb558b261354eb2bfa735
Gleison Lemos <gleison04lemos@gmail.com>
2024-07-22 21:21:43 -0300

    feat: aula 03, dia 16-07

--- 07-16/aula1-video/server.js
+++ 07-16/aula1-video/server.js
@@ -3,6 +3,10 @@ const host = '127.0.0.1';
 const port = 3000;
 
 const server = createServer((req, res) => {
+  console.log("Método/Verbos: " + req.method);
+  console.log("URL: " + req.url);
+
+
   res.writeHead(200, { 'Content-Type': 'text/html' })
   res.end(('<h1>Hello World!</h1> <br> Aula de Backend'))
 })
