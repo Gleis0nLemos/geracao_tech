c69dfd25cc69b9bb2c1364694fb66c40a8b0ead5
Gleison Lemos <gleison04lemos@gmail.com>
2024-07-23 16:44:11 -0300

    feat: aula 02, dia 17-07

--- 07-17/modulo-backend-video/aula-node/server.js
+++ 07-17/modulo-backend-video/aula-node/server.js
@@ -3,11 +3,25 @@ const { createServer } = require('node:http')
 const host = 'localhost';
 const port = 3000;
 
+// http://localhost:3000/
 const app = createServer((request, response) => {
-  console.log("Cliente solicitou...");
+  const { url, method } = request;
+
+  console.log('URL: ', url)
+  console.log('Método/Verbo: ', method)
+
+  if (url === '/') {
+    response.writeHead(200, { 'Content-Type': 'text/plain'})
+    return response.end("Olá NodeJS. Servidor Ok.");
+  } 
   
-  response.writeHead(200, { 'Content-Type': 'text/plain'})
-  return response.end("Olá NodeJS");
+  if (url === '/produtos') {
+    response.writeHead(200, { 'Content-Type': 'text/plain'})
+    return response.end("Listagem de Produtos.");
+  }
+  
+  response.writeHead(404, { 'Content-Type': 'text/plain'})
+  return response.end("Página não encontrada. - Not Found");
 })
 
 app.listen(port, host, () => {
