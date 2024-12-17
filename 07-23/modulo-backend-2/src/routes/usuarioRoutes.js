f34845a03815599f2bbcb4c8a3f971c9e0c4fb2e
Gleison Lemos <gleison04lemos@gmail.com>
2024-12-17 14:09:18 -0300

    feat: Implementação de CRUD com Express 2, dia 23/07

---
+++ 07-23/modulo-backend-2/src/routes/usuarioRoutes.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const router = express.Router();
+const usuarioController = require('../controllers/usuarioController');
+
+// Rota para listar usuários
+router.get('/', usuarioController.listar);
+
+// Rota para consultar um usuário por ID
+router.get('/:id', usuarioController.consultarPorId);
+
+// Rota para adicionar um novo usuário
+router.post('/', usuarioController.criar);
+
+// Rota para atualizar um usuário existente
+router.put('/:id', usuarioController.atualizar);
+
+// Rota para excluir um usuário
+router.delete('/:id', usuarioController.excluir);
+
+module.exports = router;
